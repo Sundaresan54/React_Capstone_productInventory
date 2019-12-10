78b13b8788a5873b807f60c7ded38aaa02d3e2af
SU351979 <sundaresan.v41@wipro.com>
2019-12-10 15:29:38 +0530

    product page static

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware,compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import Home from './components/Home'
 import Header from './components/Header';
 import reducer from './reducers/products'
+import Product from './components/Product'
+import UpdateProduct from './components/UpdateProduct'
 import * as serviceWorker from './serviceWorker';
 import './index.css'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer,composeEnhancers(applyMiddleware(thunkMiddleware)));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 ReactDOM.render(
     <Provider store={store}>
         <Router>
             <Switch>
                 <Route exact path='/' render={() => <Header><Home /></Header>}></Route>
+                <Route exact path='/product' render={() => <Header><Product /></Header>}></Route>
+                <Route exact path='/updateProduct' render={() => <Header><UpdateProduct /></Header>}></Route>
                 {/* <Route exact path='/product/:id' render={()=><Header><Quote /></Header>}></Route> */}
             </Switch>
         </Router>
