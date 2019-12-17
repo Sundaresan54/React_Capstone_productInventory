eaf7d270f9099a469c37b2c24bec7fd0e2b16a68
sathishm2408 <sathish.24081995@gmail.com>
2019-12-17 20:37:29 +0530

    modal

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 //import { Link } from 'react-router-dom';
-import {connect} from 'react-redux';
-import { Button, Header, Modal,Icon,Input } from 'semantic-ui-react';
-import {login, logout} from '../actions/users';
+import { connect } from 'react-redux';
+import { Button, Header, Modal, Icon, Input } from 'semantic-ui-react';
+import { login, logout } from '../actions/users';
 import '../index.css'
 
 class HeaderComponent extends Component {
@@ -11,63 +11,57 @@ class HeaderComponent extends Component {
         super(props);
         this.state = {
             open: false,
-            
+
         }
     }
 
-    componentDidMount(){
-        console.log("!!!!!!!!!!!! in headerrrrrr component", this.props,this.props.user);  
-       // console.log("@@@@@@@@@@ didupdate component", this.props.user.message);   
+    componentDidMount() {
+        console.log("!!!!!!!!!!!! in headerrrrrr component", this.props, this.props.user);
+        // console.log("@@@@@@@@@@ didupdate component", this.props.user.message);   
     }
 
-    show = () =>{
-        this.setState({ open: true},()=>{
+    show = () => {
+        this.setState({ open: true }, () => {
         })
-       
-    } 
-  close = () => {
-      console.log("kjhkdjhkj")
-      this.setState({ open: false })
-    }
-   
 
-    loggedInIcons=()=>{
-        return(
+    }
+    close = () => {
+        console.log("open is false")
+        this.setState({ open: false })
+    }
+
+
+    loggedInIcons = () => {
+        return (
             <ul className="navbar-nav ml-auto">
-                <li className="nav-item"><button className="nav-link loginButton" onClick={() => this.logout()}><i className="fa fa-sign-out"></i> Logout</button></li>               
+                <li className="nav-item"><button className="nav-link loginButton" onClick={() => this.logout()}><i className="fa fa-sign-out"></i> Logout</button></li>
                 <li className="nav-item"><a className="nav-link" href="/"><span className="fa fa-user"></span> {this.props.user.user.firstName}</a></li>
             </ul>
         )
     }
 
-    loginIcons=()=>{
-        return(
+    loginIcons = () => {
+        return (
             <ul className="navbar-nav ml-auto">
-            <li className="nav-item"><button className="nav-link loginButton" onClick = {()=>this.show('mini')}><i className="fa fa-sign-in"></i> Login</button></li>
-            <li className="nav-item"><a className="nav-link" href="/"><span className="fa fa-user"></span> Sign Up</a></li>
+                <li className="nav-item"><button className="nav-link loginButton" onClick={() => this.show('mini')}><i className="fa fa-sign-in"></i> Login</button></li>
+                <li className="nav-item"><a className="nav-link" href="/"><span className="fa fa-user"></span> Sign Up</a></li>
             </ul>
-            )
+        )
     }
-// componentWillReceiveProps(nextProps){
-//     console.log(nextProps.user.userData,"heello")
-//     if( nextProps.user.userData!==null|| nextProps.user.userData==undefined   ){
-//         console.log(123)
-//         this.setState({verified:'modal'})
-//     }
-// }
-     onLogin = () => {
+
+    onLogin = () => {
         const userId = document.getElementById("userId").value
         const password = document.getElementById("password").value
         //console.log("!!!!!!!!!!!!", this.props);
-        this.props.login(userId,password); 
-        
+        this.props.login(userId, password);
+
     }
 
-    loginError=()=>{
-        return(
+    loginError = () => {
+        return (
             <div className="login-row">
-        <p className='register'>{this.props.user.message}</p>
-    </div>
+                <p className='register'>{this.props.user.message}</p>
+            </div>
         )
     }
 
@@ -77,30 +71,44 @@ class HeaderComponent extends Component {
     }
 
     render() {
-        
+
         const { open } = this.state
-       // console.log("99999",this.props.user.modal);
-       console.log(this.state.open,"modal ")
-        
-       let modal =(
-           <div>
-            <Modal size="small" open={open} onClose={()=>this.close()}>
-          <Modal.Header>Delete Your Account</Modal.Header>
-          <Modal.Content>
-            <p>Are you sure you want to delete your account</p>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button negative onClick={()=>this.close()}>No</Button>
-            <Button
-              positive
-              icon='checkmark'
-              labelPosition='right'
-              content='Yes'
-            />
-          </Modal.Actions>
-        </Modal>
-           </div>
-       )
+        // console.log("99999",this.props.user.modal);
+        console.log(this.state.open, "modal ")
+
+        let modal = (
+            <div>
+
+                <Modal className="loginModal" size="small" open={open} onClose={() => this.close()}>
+                    <Modal.Header>Login</Modal.Header>
+                    <Modal.Content>
+                        <div className="login-row">
+                            <label className="label-text">User Id</label>
+                            <input type="text" id="userId" placeholder="Enter your email Id" autoFocus></input>
+                        </div>
+                        <div className="login-row">
+                            <label className="label-text">Password</label>
+                            <input type="password" id="password" placeholder="Enter your password"></input>
+                        </div>
+                        {/* <div className="login-row">
+                            </div> */}
+
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <button positive type="button" className="btn btn-outline-success login-submit" onClick={this.onLogin}>Submit</button>
+                        <button negative type="button" className="btn btn-outline-danger login-cancel" onClick={() => this.close()}>Close</button>
+
+                        {/* <Button negative onClick={() => this.close()}>No</Button>
+                        <Button
+                            positive
+                            icon='checkmark'
+                            labelPosition='right'
+                            content='Yes'
+                        /> */}
+                    </Modal.Actions>
+                </Modal>
+            </div>
+        )
         return (
             <div>
                 <nav className="navbar navbar-expand-md bg-dark navbar-dark">
@@ -123,14 +131,14 @@ class HeaderComponent extends Component {
                                 <a className="nav-link" href="/">Link</a>
                             </li>
                         </ul>
-                       
-                            {
-                                this.props.user.authenticated ? this.loggedInIcons() : this.loginIcons()
-                            }
-                            {modal}
+
+                        {
+                            this.props.user.authenticated ? this.loggedInIcons() : this.loginIcons()
+                        }
                     </div>
                 </nav>
-{/* 
+                {modal}
+                {/* 
                 <div className="modal" id="myModal">
                     <div className="modal-dialog modal-centered">
                         <div className="modal-content">
@@ -173,10 +181,10 @@ class HeaderComponent extends Component {
 
 const mapStateToProps = (state) => {
     // user : state.users
-    console.log("mapStateToProps",state);
-    return {user : state.users}
+    console.log("mapStateToProps", state);
+    return { user: state.users }
 
 }
-  
-  
-  export default connect(mapStateToProps,{login, logout})(HeaderComponent)
+
+
+export default connect(mapStateToProps, { login, logout })(HeaderComponent)
